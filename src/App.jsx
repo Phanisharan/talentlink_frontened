79468a6a4c6dc97ab5bec6a94ccfce00e3da4c17
Phanisharan <phanisharan31@gmail.com>
2025-03-02 15:38:06 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import JobPage from './pages/JobPage';
-import './app.css';
+import './App.css';
 
 
 function App() {
